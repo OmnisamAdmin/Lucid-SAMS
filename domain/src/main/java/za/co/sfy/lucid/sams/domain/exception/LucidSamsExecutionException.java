2aed9619e59bb375d24d87f4eb77647d3dc3f1ff
scmadmin <jmclaren@sfy.co.za>
2022-06-14 12:12:28 +0200

    Added eclipse configuration files and fixed warnings and errors

--- domain/src/main/java/za/co/sfy/lucid/sams/domain/exception/LucidSamsExecutionException.java
+++ domain/src/main/java/za/co/sfy/lucid/sams/domain/exception/LucidSamsExecutionException.java
@@ -3,7 +3,9 @@ package za.co.sfy.lucid.sams.domain.exception;
 
 public class LucidSamsExecutionException extends Exception{
 
-    public LucidSamsExecutionException(String message) {
+    private static final long serialVersionUID = 4568036386039187539L;
+
+	public LucidSamsExecutionException(String message) {
         super(message);
     }
 
--- resource/src/test/java/za/co/sfy/lucid/sams/repository/DataWriterRepositoryTest.java
+++ resource/src/test/java/za/co/sfy/lucid/sams/repository/DataWriterRepositoryTest.java
@@ -2,7 +2,6 @@ package za.co.sfy.lucid.sams.repository;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 //@SpringBootTest(classes = za.co.sfy.lucid.sams.repository.RecourceTestConfiguration.class)
